b6b9c725c7ece05b4346056d249501fa498daf46
Guilherme Thomas da Costa <guilhermethomas.dev@gmail.com>
2024-05-10 17:14:52 -0300

    Create a style folder

--- frontend/src/Page/Home.js
+++ frontend/src/Page/Home.js
@@ -2,8 +2,6 @@ import React from 'react'
 import styled from 'styled-components';
 
 import Hero  from '../components/Hero';
-import Display from '../components/Display';
-import Work from '../components/Work';
 import Contact from '../components/Contact';
 
 const Section = styled.div`
