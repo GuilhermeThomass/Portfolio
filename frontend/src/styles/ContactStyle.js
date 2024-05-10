3452244189b01998565add6a5092f5aeae59283d
Guilherme Thomas da Costa <guilhermethomas.dev@gmail.com>
2024-05-10 17:15:47 -0300

    Create a style folder

---
+++ frontend/src/styles/ContactStyle.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Section = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 95vh;
+  scroll-snap-align: center;
+`
+
+export const Container = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 24px;
+  color: ${props => props.theme.fontColor};
+  border: 2px solid ${props => props.theme.fontColor};
+  border-radius: 10px;
+  padding: 10px 25px;
+  background: transparent;
+`
+export const Left = styled.div`
+  width: 25vw;
+  height: 50vh;
+  gap:20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+export const Rigth = styled.div`
+  width: 15vw;
+`
+export const InputDesc = styled.textarea`
+  font-size: large;
+  height: 30vh;
+  resize: none;
+  color: ${props => props.theme.fontColor};
+  border: 2px solid ${props => props.theme.fontColor};
+  border-radius: 10px;
+  padding: 10px 25px;
+  background: transparent;
+  text-decoration: none;
+  &:active{
+    box-shadow: 2px 2px 15px ${props => props.theme.fontColor} inset;
+  }
+  &:focus{
+    outline: none;
+  }
+  &::placeholder {
+    color: ${props => props.theme.fontColor};
+    opacity: 0.5;
+  }
+`
+export const InputEmail = styled.input`
+  color: ${props => props.theme.fontColor};
+  font-size: large;
+  border: 2px solid ${props => props.theme.fontColor};
+  border-radius: 10px;
+  padding: 10px 25px;
+  background: transparent;
+  text-decoration: none;
+  &:active{
+    box-shadow: 2px 2px 15px ${props => props.theme.fontColor} inset;
+  }
+  &:focus{
+    outline: none;
+  }
+  &::placeholder {
+    color: ${props => props.theme.fontColor};
+    opacity: 0.5;
+  }
+`
+export const Titlle = styled.h1`
+`
